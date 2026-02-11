c52a9f401e4479d9368dc5d4fdbe93cb404fa8ca
Impulso <arturotlate@gmail.com>
2026-02-10 23:23:43 -0600

    Update auth.js

--- auth.js
+++ auth.js
@@ -1,37 +1,25 @@
-import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
+// auth.js
+import { auth } from "./firebase.js";
 import {
-  getAuth,
   GoogleAuthProvider,
   signInWithPopup,
   signOut,
   onAuthStateChanged
 } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js";
 
-const firebaseConfig = {
-  apiKey: "AIzaSyDOk9rqAaa8YUey1Likcyv7dtw9OrGDza0",
-  authDomain: "impulso-beta.firebaseapp.com",
-  projectId: "impulso-beta",
-  storageBucket: "impulso-beta.firebasestorage.app",
-  messagingSenderId: "284940276982",
-  appId: "1:284940276982:web:d7686b136b26546c5888d7"
-};
-
-const app = initializeApp(firebaseConfig);
-const auth = getAuth(app);
 const provider = new GoogleAuthProvider();
 
 export function login() {
-  return signInWithPopup(auth, provider);
+  signInWithPopup(auth, provider);
 }
 
 export function logout() {
-  return signOut(auth);
+  signOut(auth);
 }
 
 export function observeAuth(callback) {
   onAuthStateChanged(auth, callback);
 }
 
-export { auth };
 
 

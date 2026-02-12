8944ac06ecafe7591d818fb616f824bb043a83a0
Impulso <arturotlate@gmail.com>
2026-02-11 20:51:32 -0600

    Update auth.js

--- auth.js
+++ auth.js
@@ -1,30 +1,46 @@
 import { auth } from "./firebase.js";
 import {
   GoogleAuthProvider,
-  signInWithPopup,
+  signInWithRedirect,
+  getRedirectResult,
   signOut,
   onAuthStateChanged
 } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js";
 
 const provider = new GoogleAuthProvider();
 
+/* =========================
+   LOGIN GOOGLE (REDIRECT)
+========================= */
 export async function loginWithGoogle() {
-  try {
-    await signInWithPopup(auth, provider);
-  } catch (error) {
-    console.error("Error en login:", error);
-  }
+  await signInWithRedirect(auth, provider);
 }
 
+/* =========================
+   LOGOUT
+========================= */
 export async function logoutUser() {
   await signOut(auth);
 }
 
+/* =========================
+   OBSERVER GLOBAL
+========================= */
 export function observeAuth(callback) {
+
+  // Detecta si viene de redirect
+  getRedirectResult(auth)
+    .then(() => {})
+    .catch((error) => {
+      console.error("Redirect error:", error);
+    });
+
   onAuthStateChanged(auth, user => {
     callback(user);
   });
 }
+
+
 
 
 
